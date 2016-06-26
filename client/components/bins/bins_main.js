cdc60fb50c9070309b424301271c2cedd4149a3f
Stefan Zier <szier@icloud.com>
2016-06-25 21:07:28 -1000

    Link Navigation

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';;
 
 class BinsMain extends Component {
   render() {
+    console.log(this.props.params.binId);
     return (
       <div>Bins main</div>
     );
