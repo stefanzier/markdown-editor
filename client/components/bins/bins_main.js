2243d636d54cbb6103123636e5e8e731bf912376
Stefan Zier <szier@icloud.com>
2016-06-26 18:51:58 -1000

    Located the right bin in BinsMain

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';;
+import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Bins } from '../../../imports/collections/bins';
 
 class BinsMain extends Component {
   render() {
-    console.log(this.props.params.binId);
+    console.log(this.props.bin);
     return (
       <div>Bins main</div>
     );
   }
 }
 
-export default BinsMain;
+export default createContainer((props) => {
+  const { binId } = props.params;
+  Meteor.subscribe('bins');
+
+  return { bin: Bins.findOne(binId) };
+}, BinsMain);
